1d4730b4b3d39e0d3cedff6801ef9d5c4ecbfca8
kohta ito <kohta110@gmail.com>
2021-05-03 14:54:25 +0900

    update

--- index.js
+++ index.js
@@ -26,12 +26,21 @@ const main = async () => {
   // eventsは一定以上変化しないとデータを送っていないっぽい
   // なのでtimestampにnew Date(created_at)するとデータがまるめられてしまう
 
-  const point = new Point('natureRemo')
-    .floatField('temperature', remo.newest_events.te.val) // 温度
-    .floatField('humidity', remo.newest_events.hu.val) // 湿度
-    .floatField('illumination', remo.newest_events.il.val) // 照度
-    .floatField('movement', remo.newest_events.mo.val) // 人感センサー
+  // 温度
+  const temperature = remo.newest_events.te.val
+  // 湿度
+  const humidity = remo.newest_events.hu.val
+  // 照度
+  const illumination = remo.newest_events.il.val
+  // 人感センサー
+  const movement = remo.newest_events.mo.val
+  console.log(`temperature:${temperature}, humidity: ${humidity}, illumination: ${illumination}, movement: ${movement}`)
 
+  const point = new Point('natureRemo')
+    .floatField('temperature', temperature)
+    .floatField('humidity', humidity)
+    .floatField('illumination', illumination)
+    .floatField('movement', movement)
 
   writeApi.writePoint(point)
 
